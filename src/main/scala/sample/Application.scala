597be5db0a35d50b1707d898d4fdf079342bd723
Chris Horn <chorn4033@gmail.com>
2016-11-28 23:20:25 -0500

    cleaned up Application definition

--- src/main/scala/sample/Application.scala
+++ src/main/scala/sample/Application.scala
@@ -8,9 +8,9 @@ import scala.concurrent.ExecutionContext
 
 object Application extends App with WordCounterRoutes {
 
-  implicit val system: ActorSystem = ActorSystem()
-  implicit val materializer = ActorMaterializer()
-  implicit val ec: ExecutionContext = system.dispatcher
+  override implicit val system: ActorSystem = ActorSystem()
+  override implicit val materializer: ActorMaterializer = ActorMaterializer()
+  override implicit val ec: ExecutionContext = system.dispatcher
 
   Http().bindAndHandle(routes, "localhost", 8080)
 
